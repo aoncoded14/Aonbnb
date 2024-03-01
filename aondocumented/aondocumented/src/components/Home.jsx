21e37c78d1c5cf0bc958a354f2f52f14c4569bb2
aoncoded14 <dorkuma958@gmail.com>
2024-03-01 08:43:03 +0100

    sorted out file structure and working with hardhat

---
+++ aondocumented/aondocumented/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4  justify-center pb-20'>
+        <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Aon<span
+          className='text-yellow-400 bold'>Documented</span></h1>
+
+    </main>
+  )
+}
+
+export default Home
